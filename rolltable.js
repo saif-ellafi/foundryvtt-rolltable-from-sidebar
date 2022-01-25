69e88add3228d295eef157b98ae2ccc911b7ee4b
Saif Addin <saif1988@gmail.com>
2022-01-26 00:51:03 +0100

    Patched for V9 and added compendium rolltables support

--- rolltable.js
+++ rolltable.js
@@ -1,22 +1,45 @@
 Hooks.on("renderRollTableDirectory", (rolltables, html, data) => {
-    const tables = html.find(".entity.table")
+    const tables = html.find(".directory-item.document")
     tables.each((k) => {
         let rollIcon = document.createElement("a");
-        rollIcon.classList.add("roll-table");
-        rollIcon.setAttribute("data-action", "roll-table");
-        rollIcon.setAttribute("title", game.i18n.localize("RollTableFromSidebar.Roll"));
-        // Create roll icon
-        let die = document.createElement("i");
-        die.classList.add("fas");
-        die.classList.add("fa-dice");
-        rollIcon.appendChild(die);
-        tables[k].appendChild(rollIcon);
+        enrichRollTableSidebar(rollIcon, tables, k);
         rollIcon.addEventListener("click", RollTableFromSidebar)
     });
 });
 
+Hooks.on("renderCompendium", (rolltables, html, data) => {
+    if (data.collection.metadata.type !== 'RollTable')
+        return;
+    const tables = html.find(".directory-item.document")
+    tables.each((k) => {
+        let rollIcon = document.createElement("a");
+        enrichRollTableSidebar(rollIcon, tables, k);
+        rollIcon.addEventListener("click", (event) => RollTableFromCompendium(event, `${data.collection.metadata.package}.${data.collection.metadata.name}`))
+    });
+});
+
+function enrichRollTableSidebar(rollIcon, tables, k) {
+    rollIcon.classList.add("roll-table");
+    rollIcon.setAttribute("data-action", "roll-table");
+    rollIcon.setAttribute("title", game.i18n.localize("RollTableFromSidebar.Roll"));
+    let die = document.createElement("i");
+    die.classList.add("fas");
+    die.classList.add("fa-dice");
+    rollIcon.appendChild(die);
+    tables[k].appendChild(rollIcon);
+};
+
 function RollTableFromSidebar(event) {
-    const tableId = event.currentTarget.parentElement.dataset["entityId"];
+    const tableId = event.currentTarget.parentElement.dataset["documentId"];
     const table = game.tables.get(tableId);
     table.draw();
+}
+
+function RollTableFromCompendium(event, pack) {
+    const tableId = event.currentTarget.parentElement.dataset["documentId"];
+    console.log(tableId);
+    console.log(pack);
+    game.packs.get(pack).getDocument(tableId).then(table => {
+        table.draw();
+    })
 }
