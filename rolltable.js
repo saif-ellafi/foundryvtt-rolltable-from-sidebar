3360acc706672f0df3d6e466be73dfe5241b8557
Saif Addin <saif1988@gmail.com>
2022-01-28 12:02:13 +0100

    Roll from folders

--- rolltable.js
+++ rolltable.js
@@ -5,6 +5,12 @@ Hooks.on("renderRollTableDirectory", (rolltables, html, data) => {
         enrichRollTableSidebar(rollIcon, tables, k);
         rollIcon.addEventListener("click", RollTableFromSidebar)
     });
+    const folders = html.find(".folder-header")
+    folders.each((k) => {
+        let rollIcon = document.createElement("a");
+        enrichRollTableSidebar(rollIcon, folders, k);
+        rollIcon.addEventListener("click", RollTableFromFolder)
+    });
 });
 
 Hooks.on("renderCompendium", (rolltables, html, data) => {
@@ -37,9 +43,40 @@ function RollTableFromSidebar(event) {
 
 function RollTableFromCompendium(event, pack) {
     const tableId = event.currentTarget.parentElement.dataset["documentId"];
-    console.log(tableId);
-    console.log(pack);
     game.packs.get(pack).getDocument(tableId).then(table => {
         table.draw();
     })
+}
+
+function RollTableFromFolder(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let fid = event.target.parentElement.parentElement.parentElement.dataset.folderId;
+    let tables = game.tables.contents.filter(t => t.folder.id === fid);
+    if (tables.length > 10) {
+        let dialog = new Dialog({
+            title: 'Warning',
+            content: `<div>${game.i18n.localize("RollTableFromSidebar.Warning")}</div>`,
+            buttons: {
+                confirm: {
+                    label: 'OK',
+                    callback: () => {
+                        tables.forEach(t => {
+                            t.draw();
+                        });
+                    }
+                },
+                cancel: {
+                    label: game.i18n.localize("RollTableFromSidebar.Cancel"),
+                    callback: () => {}
+                },
+            },
+            default: 'cancel'
+        });
+        dialog.render(true);
+    } else {
+        tables.forEach(t => {
+            t.draw();
+        });
+    }
 }
